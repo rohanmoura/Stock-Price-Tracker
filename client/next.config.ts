250aa01d43960bc2c54611d3eebd03aaf6d2ece3
rohanmoura <rsmoura29@gmail.com>
2024-11-23 13:36:36 +0530

    some changes

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,16 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-        ],
-      },
-    ]
-  }
+  swcMinify: true,
 };
 
 export default nextConfig;
